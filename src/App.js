fcc4f251e0a4dc55775dbb38fe19f6e9859902cb
Sarahsegla <sarahsegla@icloud.com>
2023-03-23 14:14:27 +0000

    buttons work. added navbar, two components. router does work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React from "react";
+// import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom' ;
+import SideBar from "./Component/sidebar";
+// import About from './Component/About';
+// import GetOut from './Component/GetOut';
 
-import './App.css';
+//import './App.css';
+
 
 function App() {
   return (
+    <div>
+
+    <SideBar />
     <div className="App">
+
      We are the three Stooges - this is a test
+    </div>
+    {/* <Routes>
+      <Route path="/" element={<About />} />
+      <Route path="/GetOut" element={<GetOut />} />
+    </Routes>  */}
+
+
     </div>
   );
 }
--- src/Component/sidebar/index.js
+++ src/Component/sidebar/index.js
@@ -1,9 +1,168 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+//import InboxIcon from '@mui/icons-material/MoveToInbox';
+//import MailIcon from '@mui/icons-material/Mail';
+import About from '../About/About';
+import GetOut from '../GetOut/GetOut';
+
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
 
 
 function SideBar(props) {
 
-    return
+    
+ 
+    const theme = useTheme();
+    const [open, setOpen] = useState(false);
+    const [menuData, setMenuData] = useState("");
+
+  
+    const handleDrawerOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleDrawerClose = () => {
+      setOpen(false);
+    };
+  
+    return (
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open} style={{backgroundColor: "transparent", color: "green"}}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: 'none' }) }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Logo me please
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          {/* this is where the menus start */}
+          {/* menu 1 */}
+          <List>
+              <ListItem  disablePadding sx={{ display: 'block'}} onClick={() =>setMenuData('About')} >
+                <ListItemButton>
+                  <ListItemIcon>
+                    {/* { <MailIcon />} */}
+                  </ListItemIcon>
+                  <ListItemText primary='About' />
+                  
+                </ListItemButton>
+              </ListItem>
+{/* menu 2 */}
+              <ListItem  disablePadding sx={{ display: 'block'}} onClick={() =>setMenuData('GetOut')}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {/* { <MailIcon />}  */}
+                  </ListItemIcon>
+                  <ListItemText primary='GetOut' />
+                </ListItemButton>
+              </ListItem> 
+          </List>
+          {/* above is were the menus end */}
+          
+         
+        </Drawer>
+        <Main open={open}>
+          <DrawerHeader />
+          
+        </Main>
+        <Box >
+            {menuData === 'About' && <About />}
+            {menuData === 'GetOut' && <GetOut />}
+
+        </Box>
+      </Box>
+    );
 }
 
 export default SideBar ; 
