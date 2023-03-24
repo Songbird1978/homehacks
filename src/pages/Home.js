e6a19fdd2dae280a046b753c082670a1d3ecfa2c
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-03-24 17:53:18 +0000

    created button component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 
-
-function Home(props) {
+function Home() {
     return (
 
         <div>
             <Box height={50} />
             <h1>Home Pageeeeeeeeeeeeeeeeeeeeee</h1>
-            
         </div>
     )
 }
