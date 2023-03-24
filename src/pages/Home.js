ba5f902d987ee3c4df662d3e891a362b63667405
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-03-24 16:22:04 +0000

    created grid on homepage with background image placeholder attached to container using material UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container'
+import backgroundImage from '../images/background.jpg';
+import '../Component/grid/style.css';
+
 
 
 function Home(props) {
     return (
 
         <div>
-            <Box height={50} />
-            <h1>Home Pageeeeeeeeeeeeeeeeeeeeee</h1>
-            
+         <Container style={{ backgroundImage:`url(${backgroundImage})`, backgroundSize: 'cover'}}> 
+                <Grid className="gridItem" >
+                    <Grid item xs={12} sm={6} md={3}>1</Grid>
+                    <Grid item xs={12} sm={6} md={3}>2</Grid>
+                    <Grid item xs={12} sm={6} md={3}>3</Grid>
+                    <Grid item xs={12} sm={6} md={3}>4</Grid>
+
+                    <h1>Home Page</h1>
+                </Grid>
+                <Grid className="gridItem" >
+                    <Grid item xs={12} sm={6} md={3}>1</Grid>
+                    <Grid item xs={12} sm={6} md={3}>2</Grid>
+                    <Grid item xs={12} sm={6} md={3}>3</Grid>
+                    <Grid item xs={12} sm={6} md={3}>4</Grid>
+
+                    <h1>Home Page</h1>
+                </Grid>
+                <Grid className="gridItem" >
+                    <Grid item xs={12} sm={6} md={3}>1</Grid>
+                    <Grid item xs={12} sm={6} md={3}>2</Grid>
+                    <Grid item xs={12} sm={6} md={3}>3</Grid>
+                    <Grid item xs={12} sm={6} md={3}>4</Grid>
+
+                    <h1>Home Page</h1>
+                </Grid>
+            </Container>
         </div>
     )
 }
