eeeb414b45f8ad29d3545dde9125e85e2ac2c684
Sarahsegla <sarahsegla@icloud.com>
2023-03-25 19:35:18 +0000

    added about us title and user story

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import Box from '@mui/material/Box';
+//import Box from '@mui/material/Box';
 
 
 function About(props) {
     return (
 
         <div>
-            <Box height={50} />
-            <h1>About meeeeeeeeeeeeeeeeeeeee</h1>
-            
+            <div className='about' style={{ textAlign: 'center', color: 'green' }}>
+            <h1>About Us</h1>
+            <p> We are Home Hacks!, we help people find out different ways to save money within their houses.
+                 Find out how much it would cost to use different appliances depending on the time
+                  and how much energy they're consuming.</p>
+
+                  <h2>That's Not All!</h2>
+                  <p>Home Hacks cares about the enviroment. We help find cycling routes to help protect biodiversity.
+                    Less emissions, noise and air pollution.
+                  </p>
+            </div>
         </div>
     )
 }
