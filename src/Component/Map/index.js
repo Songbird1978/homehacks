d5c06a7baf02b12c4d00752c67c1c2fb7a31e07f
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-03-26 17:02:22 +0100

    created loading promise

---
+++ src/Component/Map/index.js
@@ -0,0 +1,29 @@
+// import { useState, useMemo, useCallback, useRef } from 'react';
+// import {
+//     GoogleMap, 
+//     Marker,
+//     DirectionsRenderer,
+//     Circle, 
+//     MarkerClusterer,
+
+// } from "@react-google-maps/api";
+// import Place from "./places";
+// import Distance from "./distance";
+
+// const LatlngLiteral = google.maps.LatlngLiteral;
+// const DirectionsResult = google.maps.DirectionsResult;
+// const MapOptions = google.maps.MapOptions;
+
+// export default function Map() {
+//     return <div>Map</div>;
+
+// }
+
+// const defaultOptions =  {
+//     strokeOpacity: 0.5,
+//     strokeWeight: 2,
+//     clickable: false,
+//     draggable: false,
+//     editable: false,
+//     visible: true,
+// };
--- src/pages/GetOut.js
+++ src/pages/GetOut.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import { useLoadScript } from "@react-google-maps/api";
-import Map from "..components/map";
+//import Map from "..components/map";
 
 
-//AIzaSyDuCwpwfpDVHr5-6Zlxz7hh1uzmu81jZ3Y
+
 
 function Getout() {
+    const { isLoaded } = useLoadScript ({
+        googleMapsApiKey: "AIzaSyDuCwpwfpDVHr5-6Zlxz7hh1uzmu81jZ3Y",
+        libraries: ["place"],
+    });
+
+    if (!isLoaded) return <div><Box height={100} />Loading...</div>;
+    
     return (
 
         <div>
             <Box height={100} />
-            <h1>Map</h1>
+            {/* <Map />; */}
             
         </div>
     )
