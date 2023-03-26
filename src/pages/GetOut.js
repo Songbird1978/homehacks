c44389794968d6264ca274523db0cd0fcb637120
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-03-26 16:31:38 +0100

    installed google maps api from react

--- src/pages/GetOut.js
+++ src/pages/GetOut.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import { useLoadScript } from "@react-google-maps/api";
+import Map from "..components/map";
 
 
-function Getout(props) {
+//AIzaSyDuCwpwfpDVHr5-6Zlxz7hh1uzmu81jZ3Y
+
+function Getout() {
     return (
 
         <div>
             <Box height={100} />
-            <h1>Get me outttttttttttttttttt!</h1>
+            <h1>Map</h1>
             
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container'
+import Container from '@mui/material/Container';
 import backgroundImage from '../images/BGI.svg';
 import '../Component/grid/style.css';
 
@@ -8,81 +8,30 @@ import '../Component/grid/style.css';
 function Home() {
     return (
         <div>
-         <Container style={{ backgroundImage:`url(${backgroundImage})`, backgroundSize: 'cover'}}> 
-                <Grid className="gridItem" >
-                    <Grid item xs={12} sm={6} md={3}>1</Grid>
-                    <Grid item xs={12} sm={6} md={3}>2</Grid>
-                    <Grid item xs={12} sm={6} md={3}>3</Grid>
-                    <Grid item xs={12} sm={6} md={3}>4</Grid>
-
-                    <h1>Home Page</h1>
+            <Container style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
+                <Grid item ></Grid>
+                <Grid container className="gridItem" >
+                    <Grid item xs={12} md={6} lg={3}>1</Grid>
+                    <Grid item xs={12} md={6} lg={3}>2</Grid>
+                    <Grid item xs={12} md={6} lg={3}>3</Grid>
+                    <Grid item xs={12} md={6} lg={3}>4</Grid>
                 </Grid>
-                <Grid className="gridItem" >
-                    <Grid item xs={12} sm={6} md={3}>1</Grid>
-                    <Grid item xs={12} sm={6} md={3}>2</Grid>
-                    <Grid item xs={12} sm={6} md={3}>3</Grid>
-                    <Grid item xs={12} sm={6} md={3}>4</Grid>
 
-                    <h1>Home Page</h1>
+                <Grid container className="gridItem" >
+                    <Grid item xs={12} md={6} lg={3}>1</Grid>
+                    <Grid item xs={12} md={6} lg={3}>2</Grid>
+                    <Grid item xs={12} md={6} lg={3}>3</Grid>
+                    <Grid item xs={12} md={6} lg={3}>4</Grid>
                 </Grid>
-                <Grid className="gridItem" >
-                    <Grid item xs={12} sm={6} md={3}>1</Grid>
-                    <Grid item xs={12} sm={6} md={3}>2</Grid>
-                    <Grid item xs={12} sm={6} md={3}>3</Grid>
-                    <Grid item xs={12} sm={6} md={3}>4</Grid>
 
-                    <h1>Home Page</h1>
-                    <Grid className="gridItem" >
-                    <Grid item xs={12} sm={6} md={3}>1</Grid>
-                    <Grid item xs={12} sm={6} md={3}>2</Grid>
-                    <Grid item xs={12} sm={6} md={3}>3</Grid>
-                    <Grid item xs={12} sm={6} md={3}>4</Grid>
-
-                    <h1>Home Page</h1>
-                </Grid>
-                <Grid className="gridItem" >
-                    <Grid item xs={12} sm={6} md={3}>1</Grid>
-                    <Grid item xs={12} sm={6} md={3}>2</Grid>
-                    <Grid item xs={12} sm={6} md={3}>3</Grid>
-                    <Grid item xs={12} sm={6} md={3}>4</Grid>
-
-                    <h1>Home Page</h1>
-                </Grid>
-                <Grid className="gridItem" >
-                    <Grid item xs={12} sm={6} md={3}>1</Grid>
-                    <Grid item xs={12} sm={6} md={3}>2</Grid>
-                    <Grid item xs={12} sm={6} md={3}>3</Grid>
-                    <Grid item xs={12} sm={6} md={3}>4</Grid>
-
-                    <h1>Home Page</h1>
-                </Grid>
-                <Grid className="gridItem" >
-                    <Grid item xs={12} sm={6} md={3}>1</Grid>
-                    <Grid item xs={12} sm={6} md={3}>2</Grid>
-                    <Grid item xs={12} sm={6} md={3}>3</Grid>
-                    <Grid item xs={12} sm={6} md={3}>4</Grid>
-
-                    <h1>Home Page</h1>
-                </Grid>
-                <Grid className="gridItem" >
-                    <Grid item xs={12} sm={6} md={3}>1</Grid>
-                    <Grid item xs={12} sm={6} md={3}>2</Grid>
-                    <Grid item xs={12} sm={6} md={3}>3</Grid>
-                    <Grid item xs={12} sm={6} md={3}>4</Grid>
-
-                    <h1>Home Page</h1>
-                </Grid>
-                <Grid className="gridItem" >
-                    <Grid item xs={12} sm={6} md={3}>1</Grid>
-                    <Grid item xs={12} sm={6} md={3}>2</Grid>
-                    <Grid item xs={12} sm={6} md={3}>3</Grid>
-                    <Grid item xs={12} sm={6} md={3}>4</Grid>
-
-                    <h1>Home Page</h1>
-                </Grid>
+                <Grid container className="gridItem" >
+                    <Grid item xs={12} md={6} lg={3}>1</Grid>
+                    <Grid item xs={12} md={6} lg={3}>2</Grid>
+                    <Grid item xs={12} md={6} lg={3}>3</Grid>
+                    <Grid item xs={12} md={6} lg={3}>4</Grid>
                 </Grid>
             </Container>
-        </div>
+        </div >
     )
 }
 
