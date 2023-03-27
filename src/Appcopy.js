e107f73cf0528b48afd842d2fe7c84ced02395df
Jojo Mukeke <jmills605@gmail.com>
2023-03-27 21:15:45 +0100

    added code for rendering but/modal

---
+++ src/Appcopy.js
@@ -0,0 +1,25 @@
+// import {React,useState} from "react";
+// // import Form from "./components/Form";
+// import Button from "./components/Button";
+// import Modal from "./components/Modal";
+// function App() {
+//   const toggle = () => setShowModal(!showModal)
+//   const [showModal, setShowModal] = useState(false)
+//   const data={
+//     description: "dsecription",
+//     title: "This is my title",
+//     svgLink: "./assets/images..."
+//   }
+//   return (
+// <>
+// <div style={{position: 'relative'}}>
+// {showModal ? <Modal data={data} toggle={toggle}/>: <><Button myFunction={toggle} content={"Show Modal"}/>;
+// </>}
+// <div>Hello</div>
+// </div>
+// </>
+
+//   )
+// 
+
+// export default App;
