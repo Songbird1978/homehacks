60fd235e939fd4edf31b63970de6f0db19345715
Jojo Mukeke <jmills605@gmail.com>
2023-03-28 19:07:13 +0100

    removed utilities from BGI

--- src/Component/modal/modal.js
+++ src/Component/modal/modal.js
@@ -6,7 +6,8 @@ function Modal (props){
     console.log(props)
     const {toggle,data} = props
     return(
-        <div style={{backgroundColor: 'gray', height: '100vh', width: '100vw', zIndex: '1', position: 'absolute', display: "flex", justifyContent:'center', alignItems:'center', flexDirection:'column'}}> Modal 
+        <div style={{backgroundColor: 'gray', height: '100vh', width: '100vw', zIndex: '1', position: 'absolute', display: "flex", 
+        justifyContent:'center', alignItems:'center', flexDirection:'column'}}> Modal 
         <Button myFunction={toggle} content={"Hide Modal"} />
         <h1>{data.title}</h1>
         <p>{data.description}</p>
