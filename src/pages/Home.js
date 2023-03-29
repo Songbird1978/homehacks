8b69c5fadb0c8d0bd4266a9e0af65916eacd22f0
Sarahsegla <sarahsegla@icloud.com>
2023-03-29 16:12:36 +0100

    still working on placement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,48 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import backgroundImage from '../images/BGI.svg';
 import '../Component/grid/style.css';
 import Logo from '../Component/logo';
-import Data from '../imageData.json';
-import '../Home.css';
-import Modal from '../Component/modal/modal';
-
 
 function Home() {
     return (
         <div>
             <Container style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
-                <Logo className="logo"></Logo>
-                
-                <Grid container item  className="gridContainer" > {
-                    Data && Data.map(dataItem => {
-                        return (
-                            <Grid item xs={12} md={6} lg={3} className="imageData" >
-                                <button className="button" onClick={ Modal } >
-                                    <img src={dataItem.imageUrl} className="image" alt=""/>
-                                </button>
-                                {/* <Modal /> */}
-                            </Grid>
-                        )
-                    })
-                }
+            <Logo className="logo"></Logo>
+
+            <Grid container className="gridItem" >
+                    <Grid item xs={12} md={5} lg={2}>1</Grid>
+                    <Grid item xs={12} md={6} lg={3}></Grid>
+                    <Grid item xs={12} md={6} lg={3}>3</Grid>
+                    <Grid item xs={12} md={6} lg={3}><img alt='wash' src="../images/cloud.svg" width={190} height={190} ></img></Grid>
+                </Grid>
+            <Grid container className="gridItem" >
+                    <Grid item xs={12} md={5} lg={2}>1</Grid>
+                    <Grid item xs={12} md={6} lg={3}></Grid>
+                    <Grid item xs={12} md={6} lg={3}>3</Grid>
+                    <Grid item xs={12} md={6} lg={3}>4</Grid>
                 </Grid>
         
                 <Grid container className="gridItem" >
-                    <Grid item xs={12} md={6} lg={3}>1</Grid>
-                    <Grid item xs={12} md={6} lg={3}>2</Grid>
+                    <Grid item xs={12} md={5} lg={2}>1</Grid>
+                    <Grid item xs={12} md={6} lg={3}></Grid>
                     <Grid item xs={12} md={6} lg={3}>3</Grid>
                     <Grid item xs={12} md={6} lg={3}>4</Grid>
+                </Grid>
+                <Grid container className="gridItem" >
+                    <Grid item xs={6} md={1} lg={1}>1</Grid>
+
+                </Grid>
+                <Grid container className="gridItem" >
+                    <Grid item xs={12} md={5} lg={2}><img alt='wash' src="../images/bike-sm.svg" width={190} height={190} ></img></Grid>
+                    <Grid item xs={12} md={6} lg={3}>2</Grid>
+                    <Grid item xs={4} md={8} lg={3}><img alt='wash' src="../images/fridgesmall.svg" width={190} height={190} ></img></Grid>
+                    
                 </Grid>
             </Container>
         </div >
