593627a6c60b37723b1213ddd1adb6a9608ff5df
Sarahsegla <sarahsegla@icloud.com>
2023-03-29 14:37:31 +0100

    logo to about img

--- src/pages/About.js
+++ src/pages/About.js
@@ -27,9 +27,9 @@ function About() {
         <p> We are Home Hacks!, we help people find out different ways to save money within their houses.</p>
         <p>Find out how much it would cost to use different appliances depending on the time
           and how much energy they're consuming.</p>
-        <p style={{ paddingTop: '100px' }}>(our logo maybe)</p>
+        <p><img alt='logo' src='../../images/logo.svg'></img></p>
 
-        <h2 style={{ paddingTop: '100px' }}>That's Not All!</h2>
+        <h2>That's Not All!</h2>
         <p>Home Hacks cares about the enviroment. We help find cycling routes to help protect biodiversity.
           Less emissions, noise and air pollution.
         </p>
