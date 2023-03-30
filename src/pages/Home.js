99910c966e2c64347110a8413163a4cbdbaf0fc1
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-03-30 11:19:47 +0100

    all images, buttons and modals and links are working. Just updating poster images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import backgroundImage from '../images/BGIcopyremoved.svg';
@@ -10,15 +10,7 @@ import '../Component/grid/style.css';
 import Logo from '../Component/logo';
 import MyProject from "../imageData.json";
 import '../Home.css';
-import washingmachine from '../images/washing-machine-sm.svg';
-import water from '../images/watertwo.svg';
-import plug from '../images/plug-sm.svg';
-import light from '../images/lights-sm.svg';
-import computer from '../images/computer-sm.svg';
-import radiator from '../images/radiator.svg';
-import bike from '../images/bike-sm.svg';
-import fridge from '../images/fridge-sm.svg';
-import kettle from '../images/kettle-sm.svg';
+
 
 const style = {
     position: "absolute",
@@ -32,101 +24,22 @@ const style = {
     p: 4,
 };
 
-// function Home(props) {
-// const [open, setOpen] = React.useState(new Array(MyProject.length).fill(false));
-// const handleClose = () => setOpen(new Array(MyProject.length).fill(false));
-
-//     return (
-//         <div>
-
-//             <Container className="bgi" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
-//                 <Logo className="logo"></Logo>
-
-//                 <Grid container className="gridContainer" spacing={6} >
-
-//                     <Grid item xs={6} md={3} lg={3} className="gridItem" >
-//                         <button className="button" onClick="" >
-//                             <img src={washingmachine} alt=""className="washingMachine"></img>
-//                         </button>
-//                     </Grid>
-//                     <Grid item xs={12} md={6} lg={3} className="gridItem" >
-//                         <button className="button" onClick="" >
-//                             <img src={water} alt=""className="water"></img>
-//                         </button>
-//                     </Grid>
-//                     <Grid item xs={12} md={6} lg={3} className="gridItem" >
-//                         <button className="button" onClick="" >
-//                             <img src={plug} alt=""className="plug"></img>
-//                         </button>
-//                     </Grid>
-//                     <Grid item xs={12} md={6} lg={3} className="gridItem" >
-//                         <button className="button" onClick="" >
-
-//                         </button>
-//                     </Grid>
-//                     <Grid item xs={12} md={6} lg={3} className="gridItem" >
-//                         <button className="button" onClick="" >
-//                             <img src={light} alt=""className="light"></img>
-//                         </button>
-//                     </Grid>
-//                     <Grid item xs={12} md={6} lg={3} className="gridItem" >
-//                         <button className="button" onClick="" >
-//                         <img src={radiator} alt=""className="radiator"></img>
-//                         </button>
-//                     </Grid>
-//                     <Grid item xs={12} md={6} lg={3} className="gridItem" >
-//                         <button className="button" onClick="" >
-//                             <img src={computer} alt=""className="computer"></img>
-//                         </button>
-//                     </Grid>
-//                     <Grid item xs={12} md={6} lg={3} className="gridItem" >
-//                             <div className="saveMoney">Save Money! Turn it off at the plug!</div>
-//                     </Grid>
-//                     <Grid item xs={12} md={6} lg={3} className="gridItem" >
-
-//                         <div className="dontDrive">ROUTES OUT OF TOWN {<br></br>} Save Money! Don't Drive!</div>
-//                         <button className="button" onClick="" >
-//                             <img src={bike} alt=""className="bike"></img>
-//                         </button>
-//                     </Grid>
-//                     <Grid item xs={12} md={6} lg={3} className="gridItem" >
-//                         <button className="button" onClick="" >
-//                             <img src={washingmachine} alt=""className="washingMachine"></img>
-//                         </button>
-//                     </Grid>
-//                     <Grid item xs={12} md={6} lg={3} className="gridItem" >
-//                         <button className="button" onClick="" >
-//                             <img src={fridge} alt=""className="fridge"></img>
-//                         </button>
-//                     </Grid>
-//                       <Grid item xs={12} md={6} lg={3} className="gridItem" >
-//                         <button className="button" onClick="" >
-//                             <img src={kettle} alt=""className="kettle"></img>
-//                         </button>
-//                     </Grid>
-//                 </Grid>
-//             </Container>
-//         </div >
-//     )
-// }
-
-// export default Home;
-
 function Home(props) {
     const [open, setOpen] = React.useState(new Array(MyProject.length).fill(false));
     const handleClose = () => setOpen(new Array(MyProject.length).fill(false));
 
     return (
         <div>
+
             <Container className="bgi" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: "cover", }}>
                 <Logo className="logo"></Logo>
 
+                <Grid container className="gridContainer" spacing={6} >
 
-                {MyProject.map((Project) => {
-                    return (
+                    {MyProject.map((Project) => {
+                        return (
 
-                        <Grid container className="gridContainer" spacing={6} key={Project.id}>
-                            <Grid item xs={6} md={3} lg={3} className="gridItem">
+                            <Grid item xs={6} md={3} lg={3} className="gridItem" key={Project.id}>
                                 <button className="button" onClick={() => {
                                     const newOpen = [...open];
                                     newOpen[Project.id - 1] = true;
@@ -154,15 +67,22 @@ function Home(props) {
                                         >
                                             {Project.description}
                                         </Typography>
+                                        <Typography
+                                            id="modal-modal-description"
+                                            sx={{ mt: 2 }}
+                                        >
+                                            {Project.info}
+                                        </Typography>
+                                        <a href={Project.links}><Button>FIND OUT MORE </Button></a>
                                     </Box>
                                 </Modal>
                             </Grid>
-                        </Grid>
-
-                    );
-                })}
 
 
+                        );
+                    })}
+
+                </Grid>
 
             </Container>
         </div>
